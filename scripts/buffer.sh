c133fc5e82e7e61d5adfcd3eb0deb27828dea1ed
lizekai-richard <lzknus@gmail.com>
2024-03-21 11:07:15 +0800

    update

--- scripts/buffer.sh
+++ scripts/buffer.sh
@@ -1,19 +1,19 @@
-CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6"
+# CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6"
 cd buffer
-nohup python buffer_DQ.py \
+nohup python buffer_CL.py \
 --dataset=CIFAR10 \
 --model=ConvNet \
 --train_epochs=100 \
 --num_experts=100 \
 --zca \
---buffer_path="../buffer_storage/convnet_loss_sorted/" \
+--buffer_path="../buffer_storage/uncertainty_sorted/" \
 --data_path="../dataset/" \
 --bins_path="../bins/" \
---sort_method="CIFAR10_ConvNet" \
+--sort_method="CIFAR10_Uncertainty" \
 --rho_max=0.01 \
 --rho_min=0.01 \
 --alpha=0.3 \
 --lr_teacher=0.01 \
 --mom=0. \
 --batch_train=256 \
-> ../logs/train_teacher_trajectories_cl_v2_convnet_loss_based.log 2>&1 &
+> ../logs/train_teacher_trajectories_cl_v4_uncertainty.log 2>&1 &
